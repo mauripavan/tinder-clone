483558ae1cad5f691233e8e18cb73b883ee59e60
mauripavan <mauriciopavan60@gmail.com>
2025-02-11 20:59:44 -0300

    refactor(eslint.config.mjs): adding eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,30 +7,87 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
+export default [
+  ...compat.extends(
     "airbnb",
     "airbnb-typescript",
     "airbnb/hooks",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-), {
+  ),
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
-
-        ecmaVersion: 5,
-        sourceType: "script",
-
-        parserOptions: {
-            project: "tsconfig.json",
-        },
+      ecmaVersion: 5,
+      sourceType: "script",
+      parserOptions: {
+        project: "tsconfig.json",
+      },
     },
-
-    rules: {},
-}];
+    rules: {
+      "import/order": [
+        "error",
+        {
+          "groups": ["builtin", "external", "internal", ["sibling", "parent", "index"]],
+          "newlines-between": "always",
+          "pathGroups": [
+            {
+              "pattern": "@types",
+              "group": "internal"
+            }
+          ],
+          "pathGroupsExcludedImportTypes": ["@types"]
+        }
+      ],
+      "react/display-name": "off",
+      "react/no-unescaped-entities": "off",
+      "react/function-component-definition": "off",
+      "import/no-anonymous-default-export": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_"
+        }
+      ],
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-use-before-define": "off",
+      "lines-around-comment": [
+        "error",
+        {
+          "beforeLineComment": true,
+          "beforeBlockComment": true,
+          "allowBlockStart": true,
+          "allowClassStart": true,
+          "allowObjectStart": true,
+          "allowArrayStart": true
+        }
+      ],
+      "newline-before-return": "error",
+      "import/newline-after-import": [
+        "error",
+        {
+          "count": 1
+        }
+      ],
+      "@typescript-eslint/ban-types": [
+        "error",
+        {
+          "extendDefaults": true,
+          "types": {
+            "{}": false
+          }
+        }
+      ]
+  
+    },
+  },
+];
