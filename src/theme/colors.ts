5f03e86f8e2c448df63095d5aab76ea8dc08a118
mauripavan <mauriciopavan60@gmail.com>
2025-02-12 22:00:57 -0300

    feat(colors): adding gradient colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,4 +2,8 @@ export const colors = {
   primary: '#FFB1C7',
   secondary: '#FEB5DB',
   tertiary: '#FF58A4',
+  gradientPrimary: ['#FF58A4', '#FF6B86'],
+  gradientSecondary: ['#00BFFF', '#32DA83'],
+  gradientTertiaty: ['#7086E3', '#9072E5'],
+  gradientQuaternary: ['#FF6B86', '#FFB03A'],
 };
