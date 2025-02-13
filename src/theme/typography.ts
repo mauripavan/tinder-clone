e6230255b338ab290cc146f286adc1a69164bcfd
mauripavan <mauriciopavan60@gmail.com>
2025-02-12 23:05:03 -0300

    feat(theme): adding typogrpahy to theme

---
+++ src/theme/typography.ts
@@ -0,0 +1,16 @@
+export const typography = {
+  fontSizes: {
+    small: 14,
+    regular: 16,
+    medium: 19,
+    large: 20,
+    xlarge: 30,
+  },
+  lineHeights: {
+    small: 19,
+    regular: 24,
+    medium: 28,
+    large: 34,
+    xlarge: 40,
+  },
+};
--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -3,5 +3,6 @@ import theme from '@theme/index';
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: typeof theme.colors;
+    typography: typeof theme.typography;
   }
 }
