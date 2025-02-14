4e6db37cb5d1e361fc23ff8dc9ff4d436c031dd5
mauripavan <mauriciopavan60@gmail.com>
2025-02-13 21:19:41 -0300

    feat: adding icons & images

---
+++ assets/icons.ts
@@ -0,0 +1,35 @@
+import CheckRound from './icons/check-round.svg';
+import Check from './icons/check.svg';
+import ChevronDown from './icons/chevron-down.svg';
+import ChevronUp from './icons/chevron-up.svg';
+import ClearThick from './icons/clear-thick.svg';
+import Clear from './icons/clear.svg';
+import Close from './icons/close.svg';
+import Exit from './icons/exit.svg';
+import HeartOutlined from './icons/heart-outlined.svg';
+import Info from './icons/info.svg';
+import Lecafe from './icons/lecafe.svg';
+import Messages from './icons/messages.svg';
+import MoreVertical from './icons/more-vertical-alt.svg';
+import Profile from './icons/profile.svg';
+import Settings from './icons/settings.svg';
+import Stories from './icons/stories.svg';
+
+export const icons = {
+  CheckRound,
+  Check,
+  ChevronDown,
+  ChevronUp,
+  ClearThick,
+  Clear,
+  Close,
+  Exit,
+  HeartOutlined,
+  Info,
+  Lecafe,
+  Messages,
+  MoreVertical,
+  Profile,
+  Settings,
+  Stories,
+};
---
+++ assets/images.ts
@@ -0,0 +1,9 @@
+import Amistad from './images/amistad.png';
+import Citas from './images/citas.png';
+import Relacion from './images/relacion.png';
+
+export const images = {
+  Amistad,
+  Citas,
+  Relacion,
+};
