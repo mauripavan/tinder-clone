2ace5cebbd2ff34fa1309f33dfe83208528fbd50
mauripavan <mauriciopavan60@gmail.com>
2025-02-14 22:38:21 -0300

    feat: wIP: homescreen updates

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,54 @@
-import React from 'react';
-import { View } from 'react-native';
-import { TextH1 } from '@components/Typography';
+import React, { useRef } from 'react';
 import Layout from '@components/Layout';
 import theme from '@theme/index';
+import Card from '@components/Card';
+import Menu from '@assets/icons/menu.svg';
+import Filter from '@assets/icons/filter.svg';
+import { MOCKED_CARDS_DATA } from '@constants/data';
+import { Animated } from 'react-native';
+import { widthPercentageToDP } from 'react-native-responsive-screen';
+
+import { CardWrapper, MainContainer, StyledAnimatedView } from './styles';
 
 function HomeScreen() {
+  const translation = useRef(new Animated.Value(0)).current;
+
+  const renderCards = () =>
+    MOCKED_CARDS_DATA.map((item, index) => {
+      const isTopCard = index === MOCKED_CARDS_DATA.length - 1;
+      const cardStyle = isTopCard
+        ? { transform: [{ translateX: translation }] }
+        : { marginTop: -18 };
+
+      const cardWidth = !isTopCard && widthPercentageToDP('75%');
+
+      return (
+        <StyledAnimatedView key={item.id} style={cardStyle}>
+          <Card
+            id={item.id}
+            key={item.id}
+            age={item.age}
+            country={item.country}
+            city={item.country}
+            image={item.image}
+            name={item.name}
+            width={cardWidth}
+          />
+        </StyledAnimatedView>
+      );
+    });
+
   return (
     <Layout
       isGradient
       gradientColor={theme.colors.gradientTertiaty}
       backgroundColor={theme.colors.primary}
     >
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <TextH1>Home Screen</TextH1>
-      </View>
+      <MainContainer>
+        <Menu fill={theme.colors.white} height={24} width={24} />
+        <Filter fill={theme.colors.white} height={24} width={24} />
+      </MainContainer>
+      <CardWrapper>{renderCards()}</CardWrapper>
     </Layout>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,9 +1,27 @@
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 import styled from 'styled-components';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 export const MainContainer = styled(View)`
-  flex: 1;
-  background-color: ${({ theme }) => theme.colors.primary};
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
+  padding-horizontal: ${wp(8)}px;
+`;
+
+export const CardWrapper = styled(View)`
+  flex: 1;
+  align-items: center;
+  margin-top: ${hp(3)}px;
+`;
+
+export const StyledAnimatedView = styled(Animated.View)`
+  position: absolute;
+  border-radius: 30px;
   justify-content: center;
+  align-items: center;
+  elevation: 3;
 `;
