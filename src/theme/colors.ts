690a1c12277164ec3f5a52052de5806347ff404c
mauripavan <mauriciopavan60@gmail.com>
2025-02-14 22:30:46 -0300

    style: update gradient color

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -4,8 +4,9 @@ export const colors = {
   primary: '#FFB1C7',
   secondary: '#FEB5DB',
   tertiary: '#FF58A4',
+  neutral: '#D0BFBF',
   gradientPrimary: ['#FF6B86', '#FF58A4'],
   gradientSecondary: ['#00BFFF', '#32DA83'],
-  gradientTertiaty: ['#7086E3', '#9072E5'],
+  gradientTertiaty: ['#9072E5', '#7086E3'],
   gradientQuaternary: ['#FFB03A', '#FF6B86'],
 };
